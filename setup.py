1b16151de38bc07d16beeedd950291eb0bdf0771
Leon <l2m2lq@gmail.com>
2019-12-25 11:29:32 +0800

    upload.

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+  long_description = fh.read()
 
 setuptools.setup(
-    name="qtciutil",
-    version="0.0.1",
-    author="l2m2",
-    author_email="l2m2lq@gmail.com",
-    description="Some basic python methods for building CI for qt projects.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/l2m2/qt-ci-util",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+  name="qtciutil",
+  version="0.0.1",
+  author="l2m2",
+  author_email="l2m2lq@gmail.com",
+  description="Some basic python methods for building CI for qt projects.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/l2m2/qt-ci-util",
+  packages=setuptools.find_packages(),
+  classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
 )
